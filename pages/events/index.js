25e10d4742aca889b9ddb6f822474a760b5b4b31
Barbora Tvrzova <btvrzova@gmail.com>
2022-09-12 16:38:22 +0100

    Layout, Header, Footer

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Layout from "../../components/Layout";
 
 const EventsPAge = () => {
   return (
-    <div>
+    <Layout title="All Events">
       <h1>Events Page</h1>
-    </div>
+    </Layout>
   );
 };
 
